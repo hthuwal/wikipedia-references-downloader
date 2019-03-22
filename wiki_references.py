173d6a2290e1f8f90ea05472951deee950308501
Harish Thuwal <hthuwal@gmail.com>
2019-03-22 20:51:56 +0530

    strip the line before printing it

--- wiki_references.py
+++ wiki_references.py
@@ -65,8 +65,9 @@ def run(file):
     """
     with open(file, "r") as f:
         for line in f:
+            line = line.strip()
             print("## %s ##" % line)
-            save_reference_pages(line.strip())
+            save_reference_pages(line)
             print("")
 
 
