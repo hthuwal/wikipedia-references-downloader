3a1e707a84f92fb3a69552cbdd9dcdec9b476c48
Harish Thuwal <hthuwal@gmail.com>
2019-03-22 15:00:15 +0530

    Handle Disambiguation Error

--- wiki_references.py
+++ wiki_references.py
@@ -1,11 +1,19 @@
 import wikipedia
 from wikipedia import DisambiguationError
 
-def get_references(keyword):
+def get_references(keyword, recurse=True):
     try:
         page = wikipedia.page(keyword)
-        return page.references
-    except DisambiguationError:
-        pass
+        return {keyword: page.references}
+  
+    except DisambiguationError as e:
+        if recurse:
+            options = wikipedia.search(keyword, results=5)  ## Get Top 5 candidates 
+            references = {}
+            for each in options:
+                references.update(get_references(each, recurse=False))
+            return references
+        else:
+            return {}
 
 print(get_references("Harish"))
