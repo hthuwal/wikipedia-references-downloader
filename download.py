a49927c0b0878b39675af9da075b20f9d3208028
Harish Thuwal <hthuwal@gmail.com>
2019-03-22 16:45:20 +0530

    Handle Exceptions and dont clean in case of pdfs

--- download.py
+++ download.py
@@ -15,17 +15,28 @@ def tag_visible(element):
     return True
 
 
-def text_from_html(url):
-    web_page = requests.get(url)
-    if web_page.status_code:
-        soup = BeautifulSoup(web_page.content, 'html.parser')
-        texts = soup.findAll(text=True)
-        visible_texts = filter(tag_visible, texts)
-        return u" ".join(t.strip() for t in visible_texts)
-    
-    return ""
+def get_data(url):
+    try:
+        web_page = requests.get(url, timeout=(5, None))
+        content_type = web_page.headers.get('content-type')
+    except Exception:
+        return None, None
 
+    if content_type is None:
+        content_type = ""
+
+    if web_page.status_code == 200:
+        if 'application/pdf' in content_type:
+            return web_page.content, "pdf"
+        else:
+            soup = BeautifulSoup(web_page.content, 'html.parser')
+            texts = soup.findAll(text=True)
+            visible_texts = filter(tag_visible, texts)
+            data = u"\n".join(t.strip() for t in visible_texts if t.strip())
+            return data, "txt"
+
+    return None, None
 
 
 if __name__ == "__main__":
-    print(text_from_html("https://www.lipsum.com/"))
+    print(get_data("https://www.lipsum.com/"))
