1e97df03ef0a96230838e841e0cd1051a4022258
Harish Thuwal <hthuwal@gmail.com>
2019-03-22 16:10:30 +0530

    Pep 8

--- wiki_references.py
+++ wiki_references.py
@@ -10,10 +10,10 @@ def get_references(keyword, recurse=True):
     try:
         page = wikipedia.page(keyword)
         return {keyword: page.references}
-  
+
     except DisambiguationError as e:
         if recurse:
-            options = wikipedia.search(keyword, results=5)  ## Get Top 5 candidates 
+            options = wikipedia.search(keyword, results=5)  # Get Top 5 candidates
             references = {}
             for each in options:
                 references.update(get_references(each, recurse=False))
