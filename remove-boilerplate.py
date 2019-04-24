cb521680537388358d0b1f2ffe040bb194712041
Harish Thuwal <hthuwal@gmail.com>
2019-04-25 00:43:24 +0530

    Pep8

--- remove-boilerplate.py
+++ remove-boilerplate.py
@@ -4,6 +4,7 @@ import click
 import shutil
 from tqdm import tqdm
 
+
 def clean(content):
     paragraphs = justext.justext(content, justext.get_stoplist("English"))
     content = ""
@@ -17,6 +18,7 @@ def write_to_file(file, content):
     with open(file, "w") as f:
         f.write(content)
 
+
 @click.command()
 @click.argument('sdir', type=click.Path(exists=True))
 def run(sdir):
@@ -27,11 +29,11 @@ def run(sdir):
         if files and os.path.basename(rdir) != 'cleaned':
             print(rdir)
             target_dir = os.path.join(rdir, "cleaned")
-            
+
             if os.path.exists(target_dir):
                 shutil.rmtree(target_dir)
             os.makedirs(target_dir)
-            
+
             for file in tqdm(files, ascii=True):
                 name, ext = os.path.splitext(file)
 
@@ -43,6 +45,7 @@ def run(sdir):
                     content = content.decode(errors='ignore')
                     cleaned_content = clean(content)
                     write_to_file(target_file, cleaned_content)
+
 
 if __name__ == "__main__":
     run()
